f1ddae94a8e522e0f13e2443e49c002d7bbf60d8
Mateus-Cypriano <cypriano.mateus@yahoo.com.br>
2024-07-01 17:03:44 -0300

    primeiro commit react

---
+++ 1_Intro/hello-world2/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+
+    <><h1> Hello World 2 on vite</h1>
+    
+    <h2> Este é um sub-titulo em React</h2></>
+
+  )
+}
+
+export default App
